c53b2eefd8a52f44db02217e9bf9d80e49794608
Joachim Bosch <bosch.joachim@outlook.com>
2025-05-09 14:56:55 +0200

    adjusted again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,32 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginNext from "eslint-config-next";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-    {
-        files: ["**/*.{js,mjs,cjs,jsx}"],
-        plugins: {
-            js: js,
-            react: pluginReact,
-        },
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
-        },
-        rules: {
-            "react/react-in-jsx-scope": "off",
-            "react/prop-types": "off",
-        },
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      js,
+      react: pluginReact,
     },
-    pluginReact.configs.flat.recommended,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+  pluginReact.configs.flat.recommended,
+  pluginNext,
 ]);
